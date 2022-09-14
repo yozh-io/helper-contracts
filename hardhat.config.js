314cc6d9797dde3e13fbed70f411942efa3f8d2d
Alina <alina.babenko@yozh.io>
2022-09-14 16:56:09 +0400

    feat: forking

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,6 +30,9 @@ module.exports = {
       url: 'http://127.0.0.1:8545',
     },
     hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
+      },
       chainId: 1,
       gasPrice: 'auto',
       accounts: {
