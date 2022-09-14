e5fbac90f3e7b78ab5196c9a672e6d3fdee891d8
Alina <alina.babenko@yozh.io>
2022-09-14 17:48:23 +0400

    feat: added networks to deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -53,5 +53,47 @@ module.exports = {
       gasPrice: 'auto',
       accounts: [`0x${privateKey}`],
     },
+    polygon: {
+      url: ` https://polygon-mainnet.g.alchemy.com/v2/${alchemyKey}`,
+      chainId: 137,
+      gasPrice: 'auto',
+      accounts: [`0x${privateKey}`],
+    },
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${alchemyKey}`,
+      chainId: 10,
+      gasPrice: 'auto',
+      accounts: [`0x${privateKey}`],
+    },
+    avalanche: {
+      url: `https://api.avax.network/ext/bc/C/rpc`,
+      chainId: 43114,
+      gasPrice: 'auto',
+      accounts: [`0x${privateKey}`],
+    },
+    arbitrumOne: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${alchemyKey}`,
+      chainId: 42161,
+      gasPrice: 'auto',
+      accounts: [`0x${privateKey}`],
+    },
+    // fantom: {
+    //   url: ``,
+    //   chainId: 250,
+    //   gasPrice: 'auto',
+    //   accounts: [`0x${privateKey}`],
+    // },
+    // gnosis: {
+    //   url: ``,
+    //   chainId: 100,
+    //   gasPrice: 'auto',
+    //   accounts: [`0x${privateKey}`],
+    // },
+    // binance: {
+    //   url: ``,
+    //   chainId: 56,
+    //   gasPrice: 'auto',
+    //   accounts: [`0x${privateKey}`],
+    // }
   },
 };
